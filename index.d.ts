56805b17c77a3af0b4ea11eb68ff417cadd90c34
Koutarou Chikuba <miz404@gmail.com>
2015-12-08 15:29:35 +0900

    Add types

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+declare module "flumt" {
+  export var mixin: {
+    dispatch: (eventName: string, ...args: any[]) => boolean;
+  };
+
+  export class Component<Props, State> {
+    refs: any;
+    props: Props;
+    state: State;
+    context: {
+      rootProps: any;
+    };
+    dispatch: (eventName: string, ...args: any[]) => boolean;
+  }
+
+  export class Flux<State> {
+    state: State;
+    on: (eventName: string, fn: Function) => void;
+    update(updater: (s: State) => State): Promise<any>;
+    subscribe(): void;
+  }
+}
