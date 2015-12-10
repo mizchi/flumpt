551eff483c543e0b1b8b780ea35cc8aa66739f7a
Koutarou Chikuba <miz404@gmail.com>
2015-12-10 18:06:38 +0900

    Implement updating queue

--- src/flumpt.js
+++ src/flumpt.js
@@ -20,6 +20,12 @@ export class Provider extends React.Component {
   }
 }
 
+const applyMiddlewares = (middlewares, nextState) => {
+  return middlewares.reduce((s, next) => {
+    return next(s);
+  }, nextState);
+}
+
 export class Component extends React.Component {
   static get contextTypes() {return SharedTypes;}
   dispatch(...args) {
@@ -47,40 +53,91 @@ export class Flux extends EventEmitter {
     this.middlewares = middlewares ? middlewares : [];
     this._renderer = createRenderer({emitter: this, render: renderer});
     this._renderedElement = null;
-    this.subscribe();
+
     this.updating = false;
-    // this._updatingQueues = []; // TODO
+    this._updatingQueues = []; // TODO
+    this._updatingPromise = null;
+
+    // setup self
+    this.subscribe();
+  }
+
+  _finishUpdate(nextState) {
+    const inAsync = !!this._updatingPromise;
+
+    if (inAsync) {
+      this._updatingQueues.length = 0;
+      this._updatingPromise = null;
+      this.updating = false;
+    }
+
+    this.state = nextState;
+    this._renderedElement = this._renderer(this.render(this.state));
+
+    if (inAsync) {
+      this.emit(":end-anync-updating");
+    }
+    return Promise.resolve();
   }
 
   update(nextStateFn) {
+    // if app is updating, add fn to queues and return current promise;
     if (this.updating) {
-      // TODO: implement queue
-      throw new Error("flumt: Update transaction is locked");
+      this._updatingQueues.push(nextStateFn);
+      return this._updatingPromise;
     }
+
+    // Create state
+    const promiseOrState = applyMiddlewares(this.middlewares, nextStateFn(this.state));
+
+    // if state is not promise, exec and resolve at once.
+    if (!(promiseOrState instanceof Promise)) {
+      const oldState = this.state;
+      this._finishUpdate(promiseOrState);
+      this.emit(":process-updating", this.state, oldState);
+      return Promise.resolve();
+    }
+
+    // start async updating!
     this.updating = true;
+    this.emit(":start-async-updating");
 
-    const applyMiddlewares = nextState => {
-      return this.middlewares.reduce((s, next) => {
-        return next(s);
-      }, nextState);
-    }
+    // create callback to response.
+    // TODO: I want Promise.defer
+    var endUpdate;
+    this._updatingPromise = new Promise(done => {
+      endUpdate = done;
+    });
 
-    const postUpdate = nextState => {
-      this.state = nextState;
-      this._renderedElement = this._renderer(this.render(this.state));
-      this.updating = false;
-    };
+    // drain first async
+    const lastState = this.state;
+    promiseOrState.then(nextState => {
+      this.emit(":process-async-updating", nextState, lastState);
 
-    const promiseOrState = applyMiddlewares(nextStateFn(this.state));
-    if (promiseOrState instanceof Promise) {
-      this.emit(":start-updating");
-      return promiseOrState.then(nextState => {
-        postUpdate(nextState);
-        this.emit(":end-updating");
-      });
-    } else {
-      postUpdate(promiseOrState);
-    }
+      // if there is left queue after first async,
+      const updateLoop = (appliedState) => {
+        const nextFn = this._updatingQueues.shift();
+        if (nextFn == null) {
+          this._finishUpdate(appliedState);
+          endUpdate();
+          return;
+        } else {
+          return Promise.resolve(
+            applyMiddlewares(
+              this.middlewares,
+              nextFn(appliedState)
+            )
+          ).then(s => {
+            this.emit(":process-async-updating", s, appliedState, this._updatingQueues.length);
+            this.emit(":process-updating", s, appliedState);
+            updateLoop(s); // recursive loop
+          });
+        }
+      }
+      updateLoop(nextState);
+    });
+
+    return this._updatingPromise;
   }
 
   _setState(...args) {
@@ -89,7 +146,7 @@ export class Flux extends EventEmitter {
     }
   }
 
-  render(props) {
+  render(state) {
     // override me
     throw "Override Me"
   }
--- test/flumpt-test.js
+++ test/flumpt-test.js
@@ -1,1 +1,95 @@
-// TODO
+import React from "react";
+import {Flux, Component} from "../src/flumpt";
+import {renderToStaticMarkup} from "react-dom/server";
+
+class Counter extends Component {
+  render() {
+    return <div>{this.props.count}</div>;
+  }
+}
+
+class TestApp extends Flux {
+  render(state) {
+    return <Counter {...state}/>;
+  }
+}
+
+// Setup renderer
+var _buffer = "";
+const app = new TestApp({
+  renderer: el => {
+    _buffer = renderToStaticMarkup(el);
+  },
+  initialState: {count: 0},
+  middlewares: [
+    (state) => {
+      Promise.resolve(state).then(s => {
+        console.log('state:', s);
+      });
+      return state
+    }
+  ]
+});
+
+function wait(ms = 100) {return new Promise(done => setTimeout(done, ms));}
+
+const {ok, equal} = require("assert");
+(async () => {
+  // case 1
+  app.update(s => ({count: 1}));
+  ok(!app.updating);
+  console.log("buffer", _buffer)
+  ok(_buffer === "<div>1</div>");
+
+  // case 2
+  const p1 = app.update(async (s) => {
+    await wait(100);
+    return {count: 2}
+  });
+
+  ok(_buffer === "<div>1</div>");
+  await p1;
+  ok(_buffer === "<div>2</div>");
+
+  // case 2
+  const p2 = app.update(async (s) => {
+    await wait(100);
+    return {count: 3}
+  });
+
+  const p3 = app.update(async (s) => {
+    await wait(100);
+    return {count: 4}
+  });
+  ok(app._updatingQueues.length === 1);
+  ok(p2 === p3);
+  await p2;
+  ok(_buffer === "<div>4</div>");
+
+  // case 2
+  const p4 = app.update(async (s) => {
+    await wait(100);
+    return {count: 3}
+  });
+
+  const p5 = app.update(async (s) => {
+    await wait(100);
+    return {count: 4}
+  });
+  ok(app._updatingQueues.length === 1);
+  const p6 = app.update(async (s) => {
+    await wait(100);
+    return {count: 2}
+  });
+  ok(p4 === p5);
+  ok(p5 === p6);
+
+  ok(app._updatingQueues.length === 2);
+  await p6;
+  ok(_buffer === "<div>2</div>");
+
+  // finish
+  console.log("pass");
+})().catch(e => {
+  throw e;
+});
